50c8f9ff9c146bd40afa9166d90c2939e26c16be
ZackRimshnick <zrimshnick@gmail.com>
2024-03-26 02:08:18 -0400

    working pretty well

--- src/js/quizGame.js
+++ src/js/quizGame.js
@@ -31,7 +31,8 @@ function changeColorOnClick(event) {
 svgContainer.addEventListener("click", changeColorOnClick);
 
 //// score container at bottom
-let currCountry = countryNames[0];
+let countryIndex = 0;
+let currCountry = countryNames[countryIndex];
 let currScore = 0;
 let currTotal = 0;
 const currCountryElement = document.getElementById(
@@ -44,23 +45,8 @@ const currTotalElement = document.getElementById("qg-game-score-total-num");
 currCountryElement.textContent = currCountry;
 currScoreElement.textContent = currScore;
 currTotalElement.textContent = currTotal;
-// function to click country
-function clickOnCountry(event) {
-  const clickedPath = event.target;
-  const pathName = event.target.getAttribute("name");
-  // Add permanentColor class to the clicked path
-  if (pathName === currCountry) {
-    clickedPath.classList.add("clickedCountryCorrect");
-    currScore = currScore + 1;
-    currScoreElement.textContent = currScore;
-    currTotal = currTotal + 1;
-    currTotalElement.textContent = currTotal;
-  } else {
-    clickedPath.classList.add("clickedCountryWrong");
-    currTotal = currTotal + 1;
-    currTotalElement.textContent = currTotal;
-  }
-}
+const gameAlert = document.getElementById("qg-game-alert");
+
 pathElements.forEach((path) => {
   path.addEventListener("click", function () {
     if (
@@ -72,10 +58,91 @@ pathElements.forEach((path) => {
       if (this.getAttribute("name") === currCountry) {
         console.log(`Clicked: ${this.getAttribute("name")} CORRECT`);
         path.classList.add("clickedCountryCorrect");
+
+        gameAlert.textContent = "CORRECT";
+        gameAlert.classList.remove("hidden");
+        gameAlert.classList.add("greened");
+        setTimeout(function () {
+          gameAlert.classList.remove("greened");
+        }, 1000);
+        gameAlert.classList.add("hidden");
+        currScore = currScore + 1;
+        currScoreElement.textContent = currScore;
+        currTotal = currTotal + 1;
+        currTotalElement.textContent = currTotal;
+        countryNames.splice(countryIndex, 1);
+        if (countryNames.length === 0) {
+          window.alert("All done with game");
+        } else {
+          if (countryIndex === countryNames.length - 1) {
+            countryIndex === 0;
+            currCountry = countryNames[countryIndex];
+            currCountryElement.textContent = currCountry;
+          } else {
+            currCountry = countryNames[countryIndex];
+            currCountryElement.textContent = currCountry;
+          }
+        }
       } else {
         console.log(`Clicked: ${this.getAttribute("name")} WRONG`);
         path.classList.add("clickedCountryWrong");
+        currTotal = currTotal + 1;
+        currTotalElement.textContent = currTotal;
+        setTimeout(function () {
+          path.classList.remove("clickedCountryWrong");
+        }, 1000);
+
+        gameAlert.textContent = "WRONG";
+        gameAlert.classList.remove("hidden");
+        gameAlert.classList.add("redded");
+        setTimeout(function () {
+          gameAlert.classList.remove("redded");
+        }, 1000);
+        gameAlert.classList.add("hidden");
+
+        pathElements.forEach((path) => {
+          if (path.getAttribute("name") === currCountry) {
+            path.classList.add("clickedCountryCorrect");
+            const flashingInterval = setInterval(() => {
+              path.classList.toggle("clickedCountryCorrect");
+            }, 500);
+            setTimeout(() => {
+              clearInterval(flashingInterval);
+              path.classList.remove("clickedCountryCorrect");
+            }, 5000);
+          }
+        });
+
+        countryNames.splice(countryIndex, 1);
+        //countryIndex = countryIndex + 1;
+        currCountry = countryNames[countryIndex];
+        currCountryElement.textContent = currCountry;
       }
     }
   });
+});
+
+const leftArrowButton = document.getElementById("qg-game-header-left-arrow");
+leftArrowButton.addEventListener("click", function () {
+  if (countryIndex === 0) {
+  } else {
+    countryIndex = countryIndex - 1;
+    currCountry = countryNames[countryIndex];
+    currCountryElement.textContent = currCountry;
+  }
+});
+const rightArrowButton = document.getElementById("qg-game-header-right-arrow");
+rightArrowButton.addEventListener("click", function () {
+  if (countryIndex === countryNames.length - 1) {
+  } else {
+    countryIndex = countryIndex + 1;
+    currCountry = countryNames[countryIndex];
+    currCountryElement.textContent = currCountry;
+    const gameAlert = document.getElementById("qg-game-alert");
+    gameAlert.textContent = "SKIPPED";
+    gameAlert.classList.add("skip");
+    setTimeout(function () {
+      gameAlert.classList.remove("skip");
+    }, 1000);
+  }
 });
